19d9803294affc96165b578fcb3352cd23d65d5b
Diego Eann <deann21@punahou.edu>
2016-08-31 12:25:52 -1000

    Update 1337anticheat.js

--- MurderScriptcraft/1337anticheat.js
+++ MurderScriptcraft/1337anticheat.js
@@ -1,5 +1,35 @@
-// var events = require('events');
-//
-// function antiCommand(event){
-//
-// }
+var events = require('events'),
+    utils = require('utils');
+var banList = [];
+var oneSlay = [],
+    oneKick = [],
+    banned = [];
+var player;
+var arrPos = 0;
+var warnings; //nts: on detecting cheat, warnings++
+    
+function execCommand( command ){
+         server.executeVanillaCommand(server, command);
+} // Allows plugin to use the command line
+
+function sessionAntiCheat(event){
+  if arrPos === 0{
+    arrPos = 0;
+  } 
+  else{
+    arrPos++;
+  }
+  player = player.event;
+  if warnings === 1 {
+    execCommand('msg @a' +player 'has been slain due to cheating'); // using msg because echo hasn't been working
+    oneSlay.push(player);
+  }
+  else if warnings === 1 {
+    execCommand('kick' +player);
+    execCommand('msg @a' +player 'has been kicked for two instances of cheating');
+  }
+  else {
+    execCommand('ban' +player);
+    execCommand('msg @a' +player 'Was banned for cheating');
+  }
+}
