84794823fc65bbf3f5248b533f7eba703d97db91
Diego Eann <deann21@punahou.edu>
2016-09-08 08:50:17 -1000

    maybe?

--- MurderScriptcraft/teststuff.js
+++ MurderScriptcraft/teststuff.js
@@ -1,14 +1,22 @@
-var events = require('events'),
-    playerName = getPlayerDisplayName();
-
+// var events = require('events'),
+//     playerName = getPlayerDisplayName();
+//
 function execCommand( command ){
        server.executeVanillaCommand(server, command);
 } // Allows plugin to use the command line
+//
+// function testIsOp() {
+//   isOp = isOp(playerName);
+//     if isOp === "true"{
+//       execCommand('msg' +isOp 'you are an op');
+//     }
+//   isOp = 0;
+// }
 
-function testIsOp() {
-  isOp = isOp(playerName);
-    if isOp === "true"{
-      execCommand('msg' +isOp 'you are an op');
-    }
-  isOp = 0;
+var utils = require("utils");
+var playernames = [require("utils").players];
+function testplayernames() {
+  for (var i = 0; i == playernames.length; i++;){
+    console.log(playernames[i]);
+  }
 }
