ede887091a9e3c47334ccaa5857fe038fb75406b
Diego Eann <deann21@punahou.edu>
2016-10-03 08:49:56 -1000

    Update serverlaunch.js

--- MurderScriptcraft/serverlaunch.js
+++ MurderScriptcraft/serverlaunch.js
@@ -2,5 +2,6 @@ var utils = require('utils');
 var events = require('events');
 var serverAddress = utils.serverAddress();
 
-function
+function launchecho{
 console.log('Server hosted on' +serverAddress 'Running v0.4a version of ScriptCraft Murder');
+}
